67030216ef52592f5a49addff8c22cdb4bc496e1
guntur <fxguntur04@gmail.com>
2025-06-08 15:27:47 +0700

    add: test multiple query parameter and multiple parameter value

--- query_param_test.go
+++ query_param_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -22,6 +23,55 @@ func TestQueryParameter(t *testing.T) {
 	recorder := httptest.NewRecorder()
 
 	SayHello(recorder, request)
+	response := recorder.Result()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+}
+
+func MultipleQueryParameter(w http.ResponseWriter, r *http.Request) {
+	firstname := r.URL.Query().Get("first_name")
+	lastname := r.URL.Query().Get("last_name")
+
+	if firstname == "" && lastname == "" {
+		fmt.Fprintf(w, "Hello")
+	} else if firstname == "" {
+		fmt.Fprintf(w, "Hello, %s", lastname)
+	} else if lastname == "" {
+		fmt.Fprintf(w, "Hello, %s", firstname)
+	} else {
+		fmt.Fprintf(w, "Hello, %s %s", firstname, lastname)
+	}
+}
+
+func TestMultipleQueryParameter(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/hello?first_name=Bobi&last_name=Doobi", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleQueryParameter(recorder, request)
+	response := recorder.Result()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+}
+
+func MultipleParameterValue(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	names := query["name"]
+	fmt.Fprint(w, strings.Join(names, " "))
+}
+
+func TestMultipleParameterValue(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/hello?name=Bobi&name=Doobi&name=Kubi", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleParameterValue(recorder, request)
 	response := recorder.Result()
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
